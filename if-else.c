324a7e0cfc6f9b0df644bc7a6f73ef76d4f027d4
MrinmoyTalukder5 <mrinmoytalukder97@gmail.com>
2022-09-17 02:31:52 -0500

    first if-else program

---
+++ if-else.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf("ente thr monry");
+ scanf("%d",&a);
+
+ if (a>400)
+  printf("\nCongrats! \n");
+else
+ printf("\nGo Work Hard!! \n");
+ return 0;   
+
+
+
+
+
+}
