ad4bb9bc8dad90c27733f1ce246b8c78de8f6032
MrinmoyTalukder5 <mrinmoytalukder97@gmail.com>
2022-09-17 02:18:34 -0500

    To check a ascii value

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Input any character to get ASCII ");
+    scanf("%c",&ch);
+    printf("ASCII value of %c is %d", ch, ch);
+    return 0;
+}
